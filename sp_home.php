a4ba64ed20154cd4345c2b013d595527fbe4243e
Pika <pikamons@gmail.com>
2015-12-02 03:09:36 +0700

    Add All File

---
+++ sp_home.php
@@ -0,0 +1,30 @@
+<?php 
+/* Template Name: SP Home */
+get_header(); ?>
+
+  <div class="row">
+    <div class="col-md-12 banner-home img-responsive"><img width="100%" height="400" src="http://www.decathlon.co.uk/ecuk/static/Delivery/home-delivery-banner.jpg"></div>
+  </div>                                
+  <div>
+    <div class="spasi"></div>
+    <div>
+      <b><div class="title">Jasa & Layanan</div></b>
+    <div class="spasi"></div>
+      <div class="row">
+        <div class="col-md-4"><img width="100%" height="200" src="images/1.png">
+          <h2>One Stop Shopping</h2>
+          <p>Kami bekerja sama dengan beberapa restaurant atau Hotel  dalam hal pemesanan dan pengaantaran catering/cake. Dalam hal ini kami juga sekaligus memasarkan produk yang dimiliki oleh pihak klien kami melalui katalog atau list product, dengan begitu pemilik restaurant mendapat dua keuntungan yaitu promosi dan operasional pengantaran yang akan di tangani oleh kami.</p>
+        </div>
+        <div class="col-md-4"><img width="100%" height="200" src="images/2.png">
+          <h2>Reguler Cash On Delivery</h2>
+          <p>Kami sadar apa artinya “URGENT” dan Kebutuhan yang perlu diantar secara cepat dalam waktu yang terbatas, layanan instant Courier kami akan langsung menjemput dan mengantar barang anda kemanapun di Jakarta secepat mungkin. Kami juga menangani distribusi undangan atau ticket untuk acara anda .</p>
+        </div>
+        <div class="col-md-4"><img width="100%" height="200" src="images/3.png">
+          <h2>Dedicated/Corporate Cervice</h2>
+          <p>Anda memiliki usaha Restaurant, Cake Shop, online Shopping atau apapun yang membutuhkan armada pengantaran. Tidak perlu repot untuk men set up delivery service. kami akan sepenuhnya membantu anda. Tidak perlu repot investasi kendaraan / motor, rekrut armada dan tidak perlu pusing memikirkan resiko, karena semua akan menjadi tanggung jawab kami.  dengan biaya yang efisien.  Disamping itu kami juga akan membantu memasarkan produk anda.</p>
+        </div>
+      </div>  
+    </div><!-- #content -->
+  </div><!-- #primary -->
+
+<?php get_footer(); ?>
