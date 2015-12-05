dbb06fc9fb0742f19439a8af80375ab8da4ddbe6
Pika <pikamons@gmail.com>
2015-12-05 21:22:43 +0700

    Link Bug Fix

--- delivery_put.php
+++ delivery_put.php
@@ -5,19 +5,17 @@ get_header(); ?>
     <div class="row">
       <h2>Pilih Restaaurant</h2>
         <?php
-          global $post, $wpdb;
-          $post_name = $post->post_name;
-          //var_dump($post);
+          global $post;
           $post_id = $post->ID;
           $template_name = get_page_template_slug($post_id);
         ?>
         <?php $content = get_distributor_by_template($template_name); ?>
-        <?php if($content!=null): ?>
+        <?php if( $content!=null ): ?>
         <?php foreach($content['distributor'] as $distributor): ?>
         <div class="col-lg-4 col-sm-6">
           <div class="thumbnail listresto">
             <a href="/template-overviews/creative" class="post-image-link">
-              <p><a href="http://localhost:81/DullatipFam/menu-list?distributor=<?php echo $distributor->id_dist; ?>"><img width="100%" height="250" src="<?php echo $distributor->gambar_dist; ?>"></a></p>
+              <p><a href="<?php echo get_home_url(); ?>/menu-list?distributor=<?php echo $distributor->id_dist; ?>"><img width="100%" height="250" src="<?php echo $distributor->gambar_dist; ?>"></a></p>
               <!-- gambar nanti diambil dari inputan admin yang menu distributor - delivery put -->
             </a>
             <div class="caption namadistributor">
