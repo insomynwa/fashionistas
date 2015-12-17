6ac45d62a5d234e1e01de292d5496a756b3cf732
Pika <pikamons@gmail.com>
2015-12-18 04:16:57 +0700

    My Profil link

--- header.php
+++ header.php
@@ -67,7 +67,7 @@
 			    <button class="btn dropdown-toggle" id="menu1" type="button" data-toggle="dropdown">Profile
 			    <span class="caret"></span></button>
 			    <ul class="dropdown-menu" role="menu" aria-labelledby="menu1">
-			     <li role="presentation"><a role="menuitem" tabindex="-1" href="#">My Profile</a></li>
+			     <li role="presentation"><a role="menuitem" tabindex="-1" href="<?php echo get_home_url().'/profil/'; ?>">My Profile</a></li>
 			      <li role="presentation"><a role="menuitem" tabindex="-1" href="<?php echo get_home_url().'/chart/'; ?>">Pesanan Saya</a></li>
 			      <li role="presentation"><a role="menuitem" tabindex="-1" href="<?php echo wp_logout_url( home_url() ); ?>">Logout</a></li>  
 			    </ul>
